501bf91b1932eddda58aa74cfd59bf10b01c7b08
Topher Winward <hi@topher.io>
2016-02-28 23:30:16 +0000

    Cleaning

--- index.js
+++ index.js
@@ -3,22 +3,22 @@
  */
 // query this with http://localhost:3000/index.html?name=joe
 
-var http = require("http");
-var url = require("url");
+var http = require( 'http' );
+var url = require( 'url' );
 
-var server = http.createServer(function (request, response) {
-  if (request.method === "GET") {
-    var queryData = url.parse(request.url, true).query;
-    response.writeHead(200, {"Content-Type": "text/plain"});
+var server = http.createServer( function ( request, response ) {
+  if ( request.method === 'GET' ) {
+    var queryData = url.parse( request.url, true ).query;
+    response.writeHead( 200, { 'Content-Type': 'text/plain' } );
     debugger;
     // if parameter is provided
-     if (queryData.name) {
-       response.end("Hello " + queryData.name + "\n");
-      } else {
-       response.end("Hello World\n");
-     }
+    if ( queryData.name ) {
+      response.end( 'Hello ' + queryData.name + '\n' );
+    } else {
+      response.end( 'Hello World\n' );
+    }
   }
-});
+} );
 
 // Listen on port 3000, IP defaults to 127.0.0.1 (localhost)
-server.listen(3000);
+server.listen( 3000 );
