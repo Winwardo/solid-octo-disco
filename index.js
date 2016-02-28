dd24d584d819b3c41ed25bba64999297c0c70d9a
Topher Winward <hi@topher.io>
2016-02-28 23:21:28 +0000

    String issue

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
  */
 // query this with http://localhost:3000/index.html?name=joe
 
-var http = require('http');
-var url = require('url');
+var http = require("http");
+var url = require("url");
 
 var server = http.createServer(function (request, response) {
-  if (request.method == 'GET') {
+  if (request.method === "GET") {
     var queryData = url.parse(request.url, true).query;
     response.writeHead(200, {"Content-Type": "text/plain"});
     debugger;
     // if parameter is provided
      if (queryData.name) {
-       response.end('Hello ' + queryData.name + '\n');
+       response.end("Hello " + queryData.name + "\n");
       } else {
        response.end("Hello World\n");
      }
