ed508a22e2e7320f681f121e5de7c0e4b547d1e8
Topher Winward <hi@topher.io>
2016-03-03 00:34:28 +0000

    Sorted terms in one tweet

--- src/client/tweetAnalysis.js
+++ src/client/tweetAnalysis.js
@@ -1,5 +1,52 @@
+'use strict';
+
+/**
+ * Given some list of tweets, create a dictionary of how often each word appears
+ * @param tweets An array of Tweet objects
+ * @returns {{}} e.g. {'hello': 5, 'world': 8}
+ */
+function countWords(tweets) {
+  const wordCount = {};
+  for (let tweet of tweets) {
+    for (let word of tweet.content.split(' ')) {
+      // ~~ will convert floats to integer,
+      // but importantly quickly convert undefined to 0
+      wordCount[word] = ~~wordCount[word] + 1;
+    }
+  };
+
+  return wordCount;
+}
+
+/**
+ * Converts some word count object to a list of word/count pairs,
+ * sorted from most frequent to least.
+ * @param wordCounts An object like {'hello': 5, 'world': 8}
+ * @returns {Array} A sorted list of word/count pairs, e.g. [{'word': 'hello', 'count': 5}, ...]
+ */
+function wordCountToSortedList(wordCounts) {
+  let result = [];
+  for (let key in wordCounts) {
+    if (wordCounts.hasOwnProperty(key)) {
+      result.push({
+        'word': key,
+        'count': wordCounts[key],
+      });
+    }
+  }
+
+  result.sort((a, b) => { return a.count < b.count; });
+  return result;
+};
+
 module.exports = {
+  /**
+   * Given a list of Tweet objects, return a sorted list of the most frequent words in them
+   * @param tweets
+   * @returns {*}
+   */
   mostFrequentWords: function (tweets) {
-    return [];
+    const wordCounts = countWords(tweets);
+    return wordCountToSortedList(wordCounts);
   },
 };
--- src/client/tweetAnalysis.spec.js
+++ src/client/tweetAnalysis.spec.js
@@ -7,5 +7,14 @@ describe('#TweetAnalysis', () => {
       const tweets = [];
       TweetAnalysis.mostFrequentWords(tweets).should.deep.equal([]);
     });
+
+    it('counts words in a single tweet, most frequent first', () => {
+      const tweets = [{ content: 'one two two three three three' }];
+      TweetAnalysis.mostFrequentWords(tweets).should.deep.equal([
+        { 'word':'three', 'count': 3 },
+        { 'word': 'two', 'count': 2 },
+        { 'word': 'one', 'count': 1 },
+      ]);
+    });
   });
 });
