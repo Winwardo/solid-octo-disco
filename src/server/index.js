2f13d56b29ff9631874923546eb023ef51e4e7c0
Topher Winward <hi@topher.io>
2016-03-03 20:08:53 +0000

    Orient test

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,9 @@
 import http from 'http';
 import url from 'url';
+import { db } from './orientdb';
+import { TweetBuilder } from '../shared/data/tweet';
+import { TweeterBuilder } from '../shared/data/tweeter';
+import { flattenImmutableObject } from '../shared/utilities';
 
 const port = process.env.PORT || 3000;
 
@@ -8,12 +12,49 @@ const server = http.createServer((request, response) => {
     const queryData = url.parse(request.url, true).query;
     response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    // if parameter is provided
-    if (queryData.name) {
-      response.end(`Hello ${queryData.name}`);
-    } else {
-      response.end('Hello World\n');
-    }
+    db.query('SELECT FROM tweet WHERE @rid="#14:1"')
+      .then((tweetRecords) => {
+        for (const tweetRecord of tweetRecords)  {
+          const tweeterId = tweetRecord.tweeter;
+          console.log('TWEET FINDING')
+          console.log('' + tweetRecord['@rid'])
+
+
+          db.query('TRAVERSE in FROM (SELECT in("TWEETED") from :id)',
+            {'params':{'id': tweetRecord['@rid']}}
+        )
+            .then((tweeterRecord_) => {
+              const tweeterRecord = tweeterRecord_[0];
+              console.log("THEN")
+              console.log(tweeterRecord)
+              console.log(tweeterRecord.name)
+
+            const tweet = TweetBuilder()
+              .content(tweetRecord.content)
+              .tweeter(
+                TweeterBuilder()
+                  .name(tweeterRecord.name)
+                  .handle(tweeterRecord.handle)
+                  .build())
+              .date(tweetRecord.date.toISOString())
+              .build();
+
+            console.log(tweet.content(), tweet.tweeter().name())
+
+            response.end(
+              JSON.stringify(
+                flattenImmutableObject(
+                  tweet)));
+
+          }).error((error) => {
+            console.log(error);
+            response.end('Failed');
+          });
+        }
+      }).error((error) => {
+        console.log(error);
+        response.end('Failed');
+      });
   }
 });
 
