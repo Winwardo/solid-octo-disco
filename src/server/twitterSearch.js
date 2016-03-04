c6c707016f067768e465bb087ee641b1b5383054
Topher Winward <hi@topher.io>
2016-03-04 21:41:09 +0000

    Twit keys quoted

--- src/server/twitterSearch.js
+++ src/server/twitterSearch.js
@@ -8,11 +8,11 @@ import { flattenImmutableObject } from '../shared/utilities';
 // These keys should be hidden in a private config file or environment variables
 // For simplicity of this assignment, they will be visible here
 var T = new Twit({
-  consumer_key:         'YiSLB0kOlsTd21UGYT32YOUgg',
-  consumer_secret:      '78b5VrGzkcIkpmftLdlFwirraelPRq2t5bFlgEcMkfaQqQh1Mb',
-  access_token:         '1831536590-kX7HPRraGcbs5t9xz1wg0QdsvbOAW4pFK5L0Y68',
-  access_token_secret:  'ceYqZAulg2MT89Jw11rA44FOwHOFcEccFv9HXFIG9ckJf',
-  timeout_ms:           60 * 1000,  // optional HTTP request timeout to apply to all requests.
+  'consumer_key':         'YiSLB0kOlsTd21UGYT32YOUgg',
+  'consumer_secret':      '78b5VrGzkcIkpmftLdlFwirraelPRq2t5bFlgEcMkfaQqQh1Mb',
+  'access_token':         '1831536590-kX7HPRraGcbs5t9xz1wg0QdsvbOAW4pFK5L0Y68',
+  'access_token_secret':  'ceYqZAulg2MT89Jw11rA44FOwHOFcEccFv9HXFIG9ckJf',
+  'timeout_ms':           60 * 1000,  // optional HTTP request timeout to apply to all requests.
 });
 
 const runQueryOnImmutableObject = (db, query, objectToFlatten) => {
