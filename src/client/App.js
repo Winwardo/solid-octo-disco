36162adad25e6125fd70e332b37d5ae7d4fa3553
Topher Winward <hi@topher.io>
2016-03-05 19:00:55 +0000

    Super cruddy basic most-used-words

--- src/client/App.js
+++ src/client/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 
+const MostUsedWords = React.createClass({
+		'getInitialState': function () {
+			return { showAllWords: true };
+		},
+		'componentDidMount': () => {
+			$(".ui.checkbox").checkbox();
+		},
+		'render': function () {
+			var self = this;
+			return (
+				<div>
+					<h3>Most frequent words</h3>
+					<div>
+						<div>
+							<div><i className="search icon"></i></div>
+							<div>
+								<label htmlFor="showAllWords">Show all words:</label>
+								<div className="ui toggle checkbox">
+									<input type="checkbox" id="showAllWords"/>
+								</div>
+							</div>
+						</div>
+						<div></div>
+					</div>
+				</div>
+			);
+		}
+	});
+
 const App = () => {
   return (
 		<div>
 			<h2 className="ui center aligned icon header">
 				<i className="circular users icon"></i>
-					Socto I win at life
+					Socto; Topher wins at life.
 			</h2>
+			<MostUsedWords></MostUsedWords>
 		</div>
 	);
 };
