85ccea881644641654e3639118599506c3fd9f7a
Topher Winward <hi@topher.io>
2016-03-05 19:17:33 +0000

    Gridded up

--- src/client/App.js
+++ src/client/App.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 const MostUsedWords = React.createClass({
 		'getInitialState': function () {
-			return { showAllWords: true };
+			return {
+				showAllWords: true,
+				'words': [
+					{ 'word': 'three', 'count': 3 },
+					{ 'word': 'two', 'count': 2 },
+					{ 'word': 'one', 'count': 1 },
+				]
+			};
 		},
 		'componentDidMount': () => {
 			$(".ui.checkbox").checkbox();
@@ -13,16 +20,36 @@ const MostUsedWords = React.createClass({
 				<div>
 					<h3>Most frequent words</h3>
 					<div>
-						<div>
-							<div><i className="search icon"></i></div>
-							<div>
+						<div className="ui two column grid">
+							<div className="column">
+								<i className="search icon"></i>
+							</div>
+							<div className='right aligned column'>
 								<label htmlFor="showAllWords">Show all words:</label>
 								<div className="ui toggle checkbox">
 									<input type="checkbox" id="showAllWords"/>
 								</div>
 							</div>
 						</div>
-						<div></div>
+						<div>
+							<div className="ui three column grid">
+								{
+									self.state.words.map((word) => {
+										return (
+											<div className="row">
+												<div className='left aligned column'>{word.word}</div>
+												<div className='right aligned column'>{word.count}</div>
+												<div className='right aligned column'>
+													<div className="ui checkbox">
+														<input type="checkbox" />
+													</div>
+												</div>
+											</div>
+										);
+									})
+								}
+							</div>
+						</div>
 					</div>
 				</div>
 			);
