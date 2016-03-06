aec2f85a33bddd513b34d13caaf546acbc81ff29
Topher Winward <hi@topher.io>
2016-03-06 15:31:13 +0000

    Searching

--- src/client/App.js
+++ src/client/App.js
@@ -5,66 +5,80 @@ const WordInfo = ({ word, count }) => (
 			<td className='left aligned column'>{word}</td>
 			<td className='right aligned column'>{count}</td>
 			<td className='right aligned column'>
-				<div className="ui checkbox" onClickTodo='TODO: Send a redux event to hide tweets with this word'>
-					<input type="checkbox" />
+				<div className="ui checkbox">
+					<input type="checkbox"  defaultChecked="true"/>
 				</div>
 			</td>
 		</tr>
 )
 
-const Words = ({words}) => (
-	<table className="ui celled table">
-		<thead>
-			<tr>
-				<th>Word</th>
-				<th>Count</th>
-				<th>Hide this word</th>
-			</tr>
-		</thead>
-		<tbody>
-			{
-				words.map((word) => {
-					return (
-						<WordInfo word={word.word} count={word.count}></WordInfo>
-					);
-				})
-			}
-		</tbody>
-	</table>
-)
+const Words = React.createClass({
+	componentDidUpdate(){
+		$(".ui.checkbox").checkbox();
+	},
+	render() {
+		return (
+			<table className="ui celled table">
+				<tbody>
+				{
+					this.props.words
+						.filter((word) => {
+							return word.word.includes(this.props.search);
+						})
+						.slice(0, 5)
+						.map((word) => {
+							return (
+								<WordInfo word={word.word} count={word.count}></WordInfo>
+							);
+						})
+				}
+				</tbody>
+			</table>
+		)
+	}
+})
 
 const CoolSearchBar = ({}) => (
 	<div>
-		<i className="search icon"></i>
-		<div className="ui input">
+		<div className="ui fluid right icon input">
 			<input type="text" placeholder="Search..." />
+			<i className="search icon"></i>
 		</div>
 	</div>
 )
 
 const MostUsedWords = React.createClass({
+		getInitialState() {
+			return {'search': ''}
+		},
 		componentDidMount() {
 			$(".ui.checkbox").checkbox();
 		},
+		changedSearch: function(e) {
+			this.setState({'search': e.target.value})
+		},
 		render() {
 			var self = this;
-			let showAll;
+			let search;
+
 			return (
 				<div>
 					<h3>Most frequent words</h3>
 					<div>
 						<div className="ui two column grid">
 							<div className="column">
-								<CoolSearchBar />
-							</div>
-							<div className='right aligned column'>
-								<label htmlFor="showAllWords">Show all words: </label>
-								<div className="ui toggle checkbox">
-									<input type="checkbox"/>
+								<div>
+									<div className="ui fluid right icon input">
+										<input type="text" placeholder="Search..." onChange={self.changedSearch}/>
+										<i className="search icon"></i>
+									</div>
 								</div>
 							</div>
+							<div className='right aligned column'>
+								<button className="ui button">Hide all</button>
+							</div>
 						</div>
-						<Words words={self.props.words} showAllWords={showAll} />
+						<Words words={self.props.words} search={self.state.search}/>
 					</div>
 				</div>
 			);
@@ -82,6 +96,17 @@ const App = () => {
 			<MostUsedWords words = {[
 					{ 'word': 'three', 'count': 3 },
 					{ 'word': 'two', 'count': 2 },
+					{ 'word': 'one', 'count': 1 },
+					{ 'word': 'ten', 'count': 1 },
+					{ 'word': 'one', 'count': 1 },
+					{ 'word': 'one', 'count': 1 },
+					{ 'word': 'one', 'count': 1 },
+					{ 'word': 'one', 'count': 1 },
+					{ 'word': 'seven', 'count': 1 },
+					{ 'word': 'one', 'count': 1 },
+					{ 'word': 'one', 'count': 1 },
+					{ 'word': 'football', 'count': 1 },
+					{ 'word': 'one', 'count': 1 },
 					{ 'word': 'one', 'count': 1 },
 				]}/>
 				</div>
