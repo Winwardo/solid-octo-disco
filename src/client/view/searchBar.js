54ae495679fbef89d4f3f8d996e15fe93b9b8cd9
Topher Winward <hi@topher.io>
2016-03-06 22:06:14 +0000

    Searchbar set prop

--- src/client/view/searchBar.js
+++ src/client/view/searchBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const SlidingSearchBar = ({ parentComp }) => (
   <div>
     <div className='ui fluid right icon input'>
-      <input type='text' placeholder='Search...' onChange={(e) => { parentComp.setState({ 'search': e.target.value }); }}/>
+      <input type='text' placeholder='Search...' onChange={(e) => { parentComp.setProp({ 'search': e.target.value }); }}/>
       <i className='search icon'></i>
     </div>
   </div>
