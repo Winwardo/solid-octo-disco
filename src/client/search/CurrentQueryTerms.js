36e3a12487da74db3ce62426ffea6c91ca057a43
Vasily Shelkov <vasilydshelkov@gmail.com>
2016-03-08 17:45:32 +0000

    Started refactoring but have train to catch !

--- src/client/search/CurrentQueryTerms.js
+++ src/client/search/CurrentQueryTerms.js
@@ -4,53 +4,61 @@ import { addSearchTerm } from './SearchActions';
 import SearchTermsList from './SearchTermsList';
 
 let nextSearchTermId = 0;
-let CurrentQueryTerms = ({ dispatch }) => {
+const CurrentQueryTerms = () => {
   let showSearchkeyword = false;
-  let searchKeyword;
+  let containerRef;
   return (
     <div className="row ui raised segment">
       <div style={{ cursor: 'text' }} onClick={() => {
         showSearchkeyword = !showSearchkeyword;
         if (showSearchkeyword) {
-          $('#searchKeywordContainer').slideDown('fast', () => {
-            searchKeyword.focus();
+          $(containerRef).slideDown('fast', () => {
+            containerRef.focus();
           });
+        } else {
+          $(containerRef).slideUp('fast');
         }
 			}}>
         <SearchTermsList />
 
-        <div id="searchKeywordContainer" className="ui fluid big transparent input"
-             style={{
-            display:'none',
-            paddingLeft:20,
-            paddingTop:10,
-            marginTop:10,
-            borderTop:1,
-            borderTop: '2px dashed #D3D5D8',
-			  }}>
-          <input id="searchKeyword" type="text" placeholder="Search a keyword or hashtag"
-                 ref={(node) => {
-              searchKeyword = node;
-						}}
-                 onBlur={() => {
-              $('#searchKeywordContainer').slideUp('fast', () => {
-                showSearchkeyword = false;
-              });
-						}}
-
-                 onKeyDown={(e) => {
-              if (e.keyCode == 13) {
-                dispatch(addSearchTerm(nextSearchTermId++, searchKeyword.value));
-                searchKeyword.value = '';
-              }
-						}}/>
-          <i className="link remove circle icon"></i>
-        </div>
+        <AddSearchTerm addSearchTermRef={node => containerRef = node}/>
       </div>
     </div>
   );
 };
 
-CurrentQueryTerms = connect()(CurrentQueryTerms);
+let AddSearchTerm = ({ dispatch, addSearchTermRef }) => {
+  const keywordSearchStyle = {
+    display:'none',
+    paddingLeft:20,
+    paddingTop:10,
+    marginTop:10,
+    borderTop:1,
+    borderTop: '2px dashed #D3D5D8',
+  }
+  return(
+    <div className="ui fluid big transparent input"
+      style={keywordSearchStyle}
+      ref={addSearchTermRef}
+      onClick={(e) => {
+        e.stopPropagation();
+      }}>
+      <input type="text" placeholder="Search a keyword or hashtag"
+        onBlur={() => {
+          $(addSearchTermRef).slideUp('fast');
+        }}
+        onKeyDown={(e) => {
+          console.log(e);
+          if (e.keyCode == 13) {
+            dispatch(addSearchTerm(nextSearchTermId++, searchKeyword.value));
+            searchKeyword.value = '';
+          }
+        }}/>
+      <i className="link remove circle icon"></i>
+    </div>
+  );
+};
+
+AddSearchTerm = connect()(AddSearchTerm);
 
 export default CurrentQueryTerms;
