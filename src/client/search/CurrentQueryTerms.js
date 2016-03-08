4b0a34aa8814a585a2ddb1a41abd8617d271e0f4
Topher Winward <hi@topher.io>
2016-03-08 15:03:42 +0000

    Revert CurrentQueryTerms

--- src/client/search/CurrentQueryTerms.js
+++ src/client/search/CurrentQueryTerms.js
@@ -7,48 +7,43 @@ let nextSearchTermId = 0;
 let CurrentQueryTerms = ({ dispatch }) => {
   let showSearchkeyword = false;
   let searchKeyword;
-
-  const keywordContainerStyle = {
-    display:'none',
-    paddingLeft:20,
-    paddingTop:10,
-    marginTop:10,
-    borderTop:1,
-    borderTop: '2px dashed #D3D5D8',
-  };
-
-  const onClick = () => {
-    showSearchkeyword = !showSearchkeyword;
-    if (showSearchkeyword) {
-      $('#searchKeywordContainer').slideDown('fast', () => {
-        searchKeyword.focus();
-      });
-    }
-  };
-
-  const onBlur = () => {
-    $('#searchKeywordContainer').slideUp('fast', () => {
-      showSearchkeyword = false;
-    });
-  };
-
-  const onKeyDown = (e) => {
-    if (e.keyCode == 13) {
-      dispatch(addSearchTerm(nextSearchTermId++, searchKeyword.value));
-      searchKeyword.value = '';
-    }
-  };
-
   return (
     <div className="row ui raised segment">
-      <div style={{ cursor: 'text' }} onClick={onClick}>
+      <div style={{ cursor: 'text' }} onClick={() => {
+        showSearchkeyword = !showSearchkeyword;
+        if (showSearchkeyword) {
+          $('#searchKeywordContainer').slideDown('fast', () => {
+            searchKeyword.focus();
+          });
+        }
+			}}>
         <SearchTermsList />
-        <div id="searchKeywordContainer" className="ui fluid big transparent input" style={keywordContainerStyle}>
-          <input id="searchKeyword" type="text" placeholder="Search a keyword or hashtag"
-            ref={(node) => { searchKeyword = node; }}
 
-            onBlur={onBlur}
-            onKeyDown={onKeyDown} />
+        <div id="searchKeywordContainer" className="ui fluid big transparent input"
+             style={{
+            display:'none',
+            paddingLeft:20,
+            paddingTop:10,
+            marginTop:10,
+            borderTop:1,
+            borderTop: '2px dashed #D3D5D8',
+			  }}>
+          <input id="searchKeyword" type="text" placeholder="Search a keyword or hashtag"
+                 ref={(node) => {
+              searchKeyword = node;
+						}}
+                 onBlur={() => {
+              $('#searchKeywordContainer').slideUp('fast', () => {
+                showSearchkeyword = false;
+              });
+						}}
+
+                 onKeyDown={(e) => {
+              if (e.keyCode == 13) {
+                dispatch(addSearchTerm(nextSearchTermId++, searchKeyword.value));
+                searchKeyword.value = '';
+              }
+						}}/>
           <i className="link remove circle icon"></i>
         </div>
       </div>
