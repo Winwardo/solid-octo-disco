1a7062d8380cc7724a2ff2c7ab61009851277aa4
Topher Winward <hi@topher.io>
2016-03-18 19:40:59 +0000

    Displaying Tweets

--- src/client/feed/Feed.js
+++ src/client/feed/Feed.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 const Feed = ({feed}) => {
-  console.log(feed);
   return (
-    <div className="ui left aligned container">
-      <div className="ui items">
+    <div>
+      <h3>Search results</h3>
+      <div className="ui divided items">
         {
           feed.posts.map((feedItem) => {return (<FeedItem content={feedItem}/>)})
         }
@@ -17,15 +18,49 @@ const Feed = ({feed}) => {
 const FeedItem = ({content}) => {
   return (
     <div className="item">
-      <div className="header">{content.author.name}</div>
-      <div className="">
-        {content.data.content}
+      <div className="meta" style={{minWidth: '40px', textAlign: 'center', verticalAlign: 'middle'}}>
+        {
+          content.source === "twitter" ? <i className="twitter icon" /> : ""
+        }
       </div>
-      <div className="extra">
-        {content.data.date}
+      <div className="core">
+        <div className="header">{content.author.name}</div>
+        <div className="content">
+          {
+            content.source === "twitter" ? <Tweet content={content} /> : ""
+          }
+        </div>
       </div>
     </div>
   )
+}
+
+class Tweet extends Component {
+  componentDidMount() {
+    $('.popup').popup();
+  }
+  render() {
+    const content = this.props.content;
+    return (
+      <div>
+        {content.data.content}
+
+        <div className="meta">
+          <span className="date">
+            {moment(content.data.date).calendar()}
+          </span>
+          |
+          <span className="likes popup" data-title="Likes">
+            <i className="like icon"/>{content.data.likes}
+          </span>
+          |
+          <span className="retweets popup" data-title="Retweets">
+            <i className="retweet icon"/>{content.data.retweets}
+          </span>
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
