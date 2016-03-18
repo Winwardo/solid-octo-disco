4931702fb42f52e7c8578097a6111294eb0ffd1c
Vasily Shelkov <vasilydshelkov@gmail.com>
2016-03-18 21:39:15 +0000

    Got deleting to work

--- src/client/search/searchReducer.js
+++ src/client/search/searchReducer.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import { ADD_SEARCH_TERM, RECEIVE_FEED_RESULTS, DELETE_SEARCH_TERM } from './searchActions';
 
 export const searchTermsReducer = (state = [], action) => {
@@ -7,12 +8,14 @@ export const searchTermsReducer = (state = [], action) => {
       ...state,
       searchTermReducer(undefined, action),
     ];
-  case DELETE_SEARCH_TERM:
-    return state.length === 1 ? [] :
-    [
-      ...state.slice(0, action.id),
-      ...state.slice(action.id + 1)
+  case DELETE_SEARCH_TERM: {
+    if (state.length === 1) return [];
+    const termIndex = state.findIndex(term => term.id === action.id);
+    return [
+      ...state.slice(0, termIndex),
+      ...state.slice(termIndex + 1)
     ];
+  }
   default:
     return state;
   }
--- src/client/search/searchReducer.spec.js
+++ src/client/search/searchReducer.spec.js
@@ -1,12 +1,13 @@
 import { should } from 'chai';
 import deepFreeze from 'deep-freeze';
 import { searchTermsReducer } from './searchReducer';
+import { ADD_SEARCH_TERM, DELETE_SEARCH_TERM } from './searchActions';
 
 describe('#SearchTermsReducer', () => {
   it('should add a hashtag search term', () => {
     const stateBefore = [];
     const action = {
-      type: 'ADD_SEARCH_TERM',
+      type: ADD_SEARCH_TERM,
       id: 0,
       query: 'Football',
       paramTypes: ['hashtag'],
@@ -34,7 +35,7 @@ describe('#SearchTermsReducer', () => {
       source: 'twitter',
     }];
     const action = {
-      type: 'ADD_SEARCH_TERM',
+      type: ADD_SEARCH_TERM,
       id: 1,
       query: 'Manchester',
       paramTypes: ['hashtag', 'author'],
@@ -47,6 +48,56 @@ describe('#SearchTermsReducer', () => {
       paramTypes: ['mention'],
       source: 'twitter',
     }, {
+      id: 1,
+      query: 'Manchester',
+      paramTypes: ['hashtag', 'author'],
+      source: 'twitter',
+    }];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    searchTermsReducer(stateBefore, action).should.deep.equal(stateAfter);
+  });
+
+  it('should return empty search terms when deleting search terms with single term', () => {
+    const stateBefore = [{
+      id: 0,
+      query: 'Football',
+      paramTypes: ['mention'],
+      source: 'twitter',
+    }];
+    const action = {
+      type: DELETE_SEARCH_TERM,
+      id: 0
+    };
+
+    const stateAfter = [];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    searchTermsReducer(stateBefore, action).should.deep.equal(stateAfter);
+  });
+
+  it('should delete term with id', () => {
+    const stateBefore = [{
+      id: 0,
+      query: 'Football',
+      paramTypes: ['mention'],
+      source: 'twitter',
+    }, {
+      id: 1,
+      query: 'Manchester',
+      paramTypes: ['hashtag', 'author'],
+      source: 'twitter',
+    }];
+    const action = {
+      type: DELETE_SEARCH_TERM,
+      id: 0
+    };
+
+    const stateAfter = [{
       id: 1,
       query: 'Manchester',
       paramTypes: ['hashtag', 'author'],
