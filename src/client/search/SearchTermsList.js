8596815779ea4964b83e6fd93ec938d188ca71a6
Vasily Shelkov <vasilydshelkov@gmail.com>
2016-03-20 18:01:08 +0000

    OnToggleClickAdded but bugged!

--- src/client/search/SearchTermsList.js
+++ src/client/search/SearchTermsList.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import TermItem from './TermItem';
-import { deleteSearchTerm } from './searchActions';
+import { deleteSearchTerm, toggleSearchTermParamTypeSelection } from './searchActions';
 
-const TermsList = ({ searchTerms, showSearchTerm, onSearchTermDeleteClick }) => (
+const TermsList = ({
+  searchTerms, showSearchTerm,
+  onSearchTermDeleteClick, onSearchTermParamTypeToggleClick
+}) => (
   <div onClick={showSearchTerm}>
     <i className="icon search"></i>
     {searchTerms.map(term => (
       <TermItem
         key={term.id}
         {...term}
+        onToggleParamTypeClick={(paramTypeToggle) => {
+          console.log(term);
+          return onSearchTermParamTypeToggleClick(term.id, paramTypeToggle);
+        }}
         onDeleteClick={() => onSearchTermDeleteClick(term.id)}
       />
     ))}
@@ -21,6 +28,9 @@ const mapStateToProps = (state) => ({ searchTerms: state.searchTerms });
 const mapDispatchToProps = (dispatch) => ({
   onSearchTermDeleteClick: (id) => {
     dispatch(deleteSearchTerm(id));
+  },
+  onSearchTermParamTypeToggleClick: (id, paramTypeName) => {
+    dispatch(toggleSearchTermParamTypeSelection(id, paramTypeName));
   }
 });
 
--- src/client/search/TermItem.js
+++ src/client/search/TermItem.js
@@ -1,53 +1,50 @@
 import React, { Component } from 'react';
 
-const TermItem = ({ onDeleteClick, query, source, paramTypes }) => {
-  let termClass = 'ui multiple compact dropdown labeled icon ';
-  switch (source) {
-  case 'twitter':
-    termClass += 'blue button';
-    break;
-  default:
-    termClass += 'button';
-  }
-
-  const paramTypeIcons = paramTypes.map((paramType) => {
-    if (paramType.icon.length > 1) {
-      return <i className={paramType.icon}></i>;
-    }
-    return <i className="icon">{paramType.icon}</i>;
-  });
-
-  return (
-    <div className={termClass} onClick={(ee) => ee.stopPropagation()}>
-      <span>
-        <i className="twitter icon"></i>
-        {query}
-      </span>
-      <i className="delete icon" onClick={() => {
-        onDeleteClick();
-      }}
-      ></i>
-      <TermItemMenu menuItemsContent={paramTypeIcons} />
-    </div>
-  );
-};
+const TermItem = ({ onDeleteClick, onToggleParamTypeClick, query, source, paramTypes }) => (
+  <div className="ui multiple compact dropdown labeled icon violet button"
+    onClick={(ee) => ee.stopPropagation()}
+  >
+    <span>
+      <i className={`${source} icon`}></i>
+      {query}
+    </span>
+    <i className="delete icon" onClick={() => {
+      onDeleteClick();
+    }}
+    ></i>
+    <TermItemMenu
+      paramTypes={paramTypes}
+      onToggleParamType={(paramTypeToggle) => onToggleParamTypeClick(paramTypeToggle)}
+    />
+  </div>
+);
 
 class TermItemMenu extends Component {
   componentDidMount() {
     $('.ui.dropdown')
       .dropdown({
-        onChange: (value, text, $selectedItem) => {
-
+        action: (value, text) => {
+          this.props.onToggleParamType(text);
         }
       });
   }
 
   render() {
-    const menuItems = this.props.menuItemsContent.map((content, id) => (
-      <div key={id} className="item">
-        {content}
-      </div>
-    ));
+    const menuItems = this.props.paramTypes.map((paramType, id) => {
+      let content;
+      if (paramType.icon.length > 1) {
+        content = <i className={paramType.icon}></i>;
+      } else {
+        content = <i className="icon">{paramType.icon}</i>;
+      }
+      return (
+        <div key={id} data-value={paramType.name} className="item">
+          {content} HELLO
+          {id}
+          {paramType.name}
+        </div>
+      );
+    });
 
     return (
       <div className="menu">
