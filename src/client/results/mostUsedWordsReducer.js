7baf2a5607ae80b77aef72ed4575fa45150c1cd9
Topher Winward <hi@topher.io>
2016-03-23 00:21:36 +0000

    Pass immutable tests

--- src/client/results/mostUsedWordsReducer.js
+++ src/client/results/mostUsedWordsReducer.js
@@ -1,22 +1,21 @@
 import { HIDE_MOST_USED_WORD, SHOW_MOST_USED_WORD } from './mostUsedWordsActions';
 
 const MostUsedWordsReducer = (state = { filterTerm: '', wordsToHide: {} }, action) => {
-  console.log("heyyy hide", action)
   switch (action.type) {
     case 'UPDATE_SEARCH_MOST_USED_WORDS_FILTER':
       return { ...state, filterTerm: action.filterTerm };
     case HIDE_MOST_USED_WORD:
       {
-        const result = {...state};
+        const result = { ...state, wordsToHide: Object.assign({}, state.wordsToHide) };
         result.wordsToHide[action.word] = true;
         return result;
-      };
+      }
     case SHOW_MOST_USED_WORD:
       {
-        const result = {...state};
+        const result = { ...state, wordsToHide: Object.assign({}, state.wordsToHide) };
         delete result.wordsToHide[action.word];
         return result;
-      };
+      }
     default:
       return state;
   };
