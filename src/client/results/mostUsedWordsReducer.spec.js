774de45ffc9d75102db74838da9630271b1969cd
Topher Winward <hi@topher.io>
2016-03-23 00:18:29 +0000

    Tests

--- src/client/results/mostUsedWordsReducer.spec.js
+++ src/client/results/mostUsedWordsReducer.spec.js
@@ -1,6 +1,7 @@
 import { should } from 'chai';
 import deepFreeze from 'deep-freeze';
 import MostUsedTermsReducer from './mostUsedWordsReducer';
+import { HIDE_MOST_USED_WORD, SHOW_MOST_USED_WORD } from './mostUsedWordsActions';
 
 describe('#MostUsedWordsReducer', () => {
   it('should add the given search term', () => {
@@ -11,12 +12,125 @@ describe('#MostUsedWordsReducer', () => {
     };
 
     const stateAfter = {
-      'filterTerm': 'Football',
+      filterTerm: 'Football',
     };
 
     deepFreeze(stateBefore);
     deepFreeze(action);
 
     MostUsedTermsReducer(stateBefore, action).should.deep.equal(stateAfter);
+  });
+
+  describe('Hiding words', () => {
+    it('should add a hidden word', () => {
+      const stateBefore = {
+        wordsToHide: {}
+      };
+      const action = {
+        type: HIDE_MOST_USED_WORD,
+        word: 'LeMoN',
+      };
+
+      const stateAfter = {
+        wordsToHide: {
+          LeMoN: true
+        }
+      };
+
+      //deepFreeze(stateBefore);
+      //deepFreeze(action);
+
+      MostUsedTermsReducer(stateBefore, action).should.deep.equal(stateAfter);
+    });
+
+    it('should not conflate similar hidden words', () => {
+      const stateBefore = {
+        wordsToHide: {
+          lEmOn: true,
+        }
+      };
+      const action = {
+        type: HIDE_MOST_USED_WORD,
+        word: 'LeMoN',
+      };
+
+      const stateAfter = {
+        wordsToHide: {
+          lEmOn: true,
+          LeMoN: true,
+        }
+      };
+
+      //deepFreeze(stateBefore);
+      //deepFreeze(action);
+
+      MostUsedTermsReducer(stateBefore, action).should.deep.equal(stateAfter);
+    });
+
+    it('should stay the same if trying to hide an already hidden word', () => {
+      const stateBefore = {
+        wordsToHide: {
+          LEMON: true
+        }
+      };
+      const action = {
+        type: HIDE_MOST_USED_WORD,
+        word: 'LEMON',
+      };
+
+      const stateAfter = {
+        wordsToHide: {
+          LEMON: true
+        }
+      };
+
+      //deepFreeze(stateBefore);
+      //deepFreeze(action);
+
+      MostUsedTermsReducer(stateBefore, action).should.deep.equal(stateAfter);
+    });
+
+    it('should show/remove a hidden word', () => {
+      const stateBefore = {
+        wordsToHide: {
+          LEMON: true
+        }
+      };
+      const action = {
+        type: SHOW_MOST_USED_WORD,
+        word: 'LEMON',
+      };
+
+      const stateAfter = {
+        wordsToHide: {
+        }
+      };
+
+      //deepFreeze(stateBefore);
+      //deepFreeze(action);
+
+      MostUsedTermsReducer(stateBefore, action).should.deep.equal(stateAfter);
+    });
+
+    it('should stay the same if trying to show a non-hidden word', () => {
+      const stateBefore = {
+        wordsToHide: {
+        }
+      };
+      const action = {
+        type: SHOW_MOST_USED_WORD,
+        word: 'LEMON',
+      };
+
+      const stateAfter = {
+        wordsToHide: {
+        }
+      };
+
+      //deepFreeze(stateBefore);
+      //deepFreeze(action);
+
+      MostUsedTermsReducer(stateBefore, action).should.deep.equal(stateAfter);
+    });
   });
 });
