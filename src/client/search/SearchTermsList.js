e75e5a3b3fa296bfa120922987a78a17091998c8
Vasily Shelkov <vasilydshelkov@gmail.com>
2016-04-02 12:08:20 +0100

    Works with add/remove SearchTerm
    but has stopped toggleParamTypeClick from work

--- src/client/search/SearchTermsList.js
+++ src/client/search/SearchTermsList.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import TermItem from './TermItem';
-import { deleteSearchTerm, toggleSearchTermParamTypeSelection } from './searchActions';
+import {
+  deleteSearchTerm, invalidateFeedResults, toggleSearchTermParamTypeSelection
+} from './searchActions';
 
-const TermsList = ({
-  searchTerms, showSearchTerm,
-  onSearchTermDeleteClick, onSearchTermParamTypeToggleClick,
-}) => (
-  <div onClick={showSearchTerm}>
-    <i className="icon search"></i>
-    {searchTerms.map(term => (
-      <TermItem
-        onToggleParamTypeClick={(paramTypeToggle) =>
-          onSearchTermParamTypeToggleClick(term.id, paramTypeToggle)
-        }
-        key={term.id}
-        {...term}
-        onDeleteClick={() => onSearchTermDeleteClick(term.id)}
-      />
-    ))}
-  </div>
-);
+class TermsList extends Component {
+  componentWillReceiveProps() {
+    this.props.onSearchTermsChange();
+  }
+
+  render() {
+    return (
+      <div onClick={this.props.showSearchTerm}>
+        <i className="icon search"></i>
+        {this.props.searchTerms.map(term => (
+          <TermItem
+            onToggleParamTypeClick={(paramTypeToggle) =>
+              this.props.onSearchTermParamTypeToggleClick(term.id, paramTypeToggle)
+            }
+            key={term.id}
+            {...term}
+            onDeleteClick={() => this.props.onSearchTermDeleteClick(term.id)}
+          />
+        ))}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({ searchTerms: state.searchTerms });
 
@@ -28,10 +35,12 @@ const mapDispatchToProps = (dispatch) => ({
   onSearchTermParamTypeToggleClick: (id, paramTypeName) => {
     dispatch(toggleSearchTermParamTypeSelection(id, paramTypeName));
   },
-
   onSearchTermDeleteClick: (id) => {
     dispatch(deleteSearchTerm(id));
   },
+  onSearchTermsChange: () => {
+    dispatch(invalidateFeedResults());
+  }
 });
 
 export default connect(
