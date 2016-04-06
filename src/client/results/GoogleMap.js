79c3c8be5d1c206baa710affc397ec76440018e3
Topher Winward <hi@topher.io>
2016-04-06 13:11:59 +0100

    Component syntax

--- src/client/results/GoogleMap.js
+++ src/client/results/GoogleMap.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
 
-export default React.createClass({
+export default class GoogleMap extends Component {
   // Inspired by http://revelry.co/google-maps-react/
-  render: () => (
-    <div />
-  ),
 
-  componentDidUpdate: function() {
+  render() { return (<div />) };
+
+  componentDidUpdate() {
     this.markerBounds = new google.maps.LatLngBounds();
     this.markerBounds.extend(new google.maps.LatLng(49.9, -9.8)) // South West of UK
     this.markerBounds.extend(new google.maps.LatLng(58.9, -1.93)) // North East of UK
 
     this.markers.forEach((marker) => marker.setMap(null));
-    this.props.posts.forEach(this.addMarker);
+    this.props.posts.forEach((post) => this.addMarker.bind(this)(post));
 
     // Fit the zoom of the map to show all of the marked tweets, and no more.
     this.map.fitBounds(this.markerBounds);
-  },
+  }
 
-  componentDidMount: function() {
+  componentDidMount() {
     this.markers = [];
     this.map = this.createMap();
 
     this.componentDidUpdate();
-  },
+  }
 
-  createMap: function() {
+  createMap() {
     const mapOptions = {
       minZoom: 1,
       zoom: 1,
@@ -34,9 +33,9 @@ export default React.createClass({
     }
 
     return new google.maps.Map($("#tweetMap")[0], mapOptions);
-  },
+  }
 
-  addMarker: function(post) {
+  addMarker(post) {
     const pos = new google.maps.LatLng(post.data.latitude, post.data.longitude);
 
     const marker = new google.maps.Marker({
@@ -57,5 +56,5 @@ export default React.createClass({
     this.markerBounds.extend(pos)
 
     return marker;
-  },
-});
+  }
+};
